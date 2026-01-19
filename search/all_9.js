51731d8ee1e5900de35440f60ffbf7679568aa62
hasenradball <hasenradball@users.noreply.github.com>
2026-01-19 09:05:42 +0000

    deploy: 7bfae6acd47341cee01093dc2d63b3d11c3f161d

--- search/all_9.js
+++ search/all_9.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['setpin_0',['setPin',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#aafcbac7751765f3af2b57b0d3b803285',1,'PCF8574_I2C::PCF8574']]],
   ['setport_1',['setPort',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#ae280d8a18f08bc51de391afa55dcf3f9',1,'PCF8574_I2C::PCF8574']]],
-  ['status_20codes_20of_20pcf8574_2',['Status Codes of PCF8574',['../md__r_e_a_d_m_e.html#autotoc_md6',1,'']]]
+  ['shiftleft_2',['shiftLeft',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#a0226fa90971aa90b3d5e5271e834c5a9',1,'PCF8574_I2C::PCF8574']]],
+  ['shiftright_3',['shiftRight',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#ac3e0b0395f1d57e2f925109365e08b0b',1,'PCF8574_I2C::PCF8574']]],
+  ['status_20codes_20of_20pcf8574_4',['Status Codes of PCF8574',['../md__r_e_a_d_m_e.html#autotoc_md6',1,'']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['setpin_0',['setPin',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#aafcbac7751765f3af2b57b0d3b803285',1,'PCF8574_I2C::PCF8574']]],
-  ['setport_1',['setPort',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#ae280d8a18f08bc51de391afa55dcf3f9',1,'PCF8574_I2C::PCF8574']]]
+  ['setport_1',['setPort',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#ae280d8a18f08bc51de391afa55dcf3f9',1,'PCF8574_I2C::PCF8574']]],
+  ['shiftleft_2',['shiftLeft',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#a0226fa90971aa90b3d5e5271e834c5a9',1,'PCF8574_I2C::PCF8574']]],
+  ['shiftright_3',['shiftRight',['../class_p_c_f8574___i2_c_1_1_p_c_f8574.html#ac3e0b0395f1d57e2f925109365e08b0b',1,'PCF8574_I2C::PCF8574']]]
 ];
